e5e086063805311d10af1bab69fbdeb8250ad19f
shokheg <ioga@bk.ru>
2022-06-14 20:25:13 +0300

    Add solution v1.0

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,14 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author shokhalevich
+ */
+@Component
+public class Dog extends Animal{
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+}
